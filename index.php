a72966817cbf87785bce0ea41d1a0da274bc263c
anthonyreynaldi <77012214+anthonyreynaldi@users.noreply.github.com>
2022-09-26 22:18:15 +0700

    add vision

--- index.php
+++ index.php
@@ -82,6 +82,22 @@
     <!-- gap -->
     <div class="my-5 text-white">.</div>
 
+    <div class="container-fluid p-0" id="vision">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L80,165.3C160,171,320,181,480,170.7C640,160,800,128,960,122.7C1120,117,1280,139,1360,149.3L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+        
+        <div class="row justify-content-end me-1">
+            <div class="col-sm-4 me-5 bg-dark text-white text-center rounded-5 opacity-75 p-4" data-aos="flip-down">
+                <h1 class="text-center mb-4" data-aos="zoom-in" data-aos-delay="500">Our Mission & Vision</h1>
+                <h5 data-aos="zoom-in" data-aos-delay="1000">
+                    Our Mission is to make a better waste management through our product while also Educating the users contribute to a better world with the better waste management
+                </h5>
+
+            </div>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,256L80,266.7C160,277,320,299,480,298.7C640,299,800,277,960,261.3C1120,245,1280,235,1360,229.3L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+    </div>
+
     <h1 class="text-center my-5" data-aos="zoom-in">Intie Kategori nya apa</h1>
 
     <!-- type for desktop -->
@@ -327,6 +343,12 @@
         filter: grayscale(0);
         transform: scale(1.1);
     }
+
+    #vision{
+        background-image: url('img/tpa-red.png');
+        background-size: cover;
+    }
+    
 </style>
 <script>
     AOS.init();
